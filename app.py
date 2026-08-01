d8f47971c81dc80ab98f3facf03d7173611bca74
TOKEN7bil <damboorkennedy@gmail.com>
2026-08-01 00:40:36 +0000

    Fix formatting and ensure proper execution of app.py

--- app.py
+++ app.py
@@ -40,4 +40,4 @@ def ussd():
         else: response = "END Invalid input"
     return response
 
-if name == "__main__": app.run(host='0.0.0.0', port=10000)
+if __name__ == "__main__": app.run(host='0.0.0.0', port=10000)
